e6b0e6e6725cac4c2e18c1ecc783e060df07c94a
Johexlimee <jhcastillo@uniempresarial.edu.co>
2023-02-17 19:17:19 -0500

    Feb-17

--- CalculoTotalJS/scripts/main.js
+++ CalculoTotalJS/scripts/main.js
@@ -8,7 +8,25 @@ btnCalcular.onclick = function (){
     if (cantidad >= 1 && color !=0){
         document.getElementById('total').innerText=`Total: $${cantidad*400000}`;
         document.getElementById('cant').innerText=`Cantidad: ${cantidad}`;
-        //document.getElementById('color').value="";
+        
+        switch (color){
+            case "rojo":
+                document.getElementById('seeColor').innerText="Rojo claro";
+                document.getElementById('divColor').style.backgroundColor='lightcoral';
+            break;
+            case "verde":
+                document.getElementById('seeColor').innerText="Verde Mate";
+            break;
+            case "azul":
+                document.getElementById('seeColor').innerText="Azul Oscuro";
+            break;
+            case "negro":
+                document.getElementById('seeColor').innerText="Negro";
+            break;
+            case "blanco":
+                document.getElementById('seeColor').innerText="Blanco";
+            break;
+        }
     } else {
         alert("Datos incorrectos, Revisar campos");
     }
